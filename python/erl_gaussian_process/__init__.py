348774116208c65e3505a4d41f364fed4ebec6ee
Zhirui Dai <daizhirui@hotmail.com>
2024-07-26 00:53:36 -0700

    update python

--- python/erl_gaussian_process/__init__.py
+++ python/erl_gaussian_process/__init__.py
@@ -1,6 +1,7 @@
 # import pybind dependencies
 import erl_common as common
 import erl_covariance as covariance
+import erl_geometry as geometry
 
 # import package modules
 from .pyerl_gaussian_process import *
@@ -8,6 +9,7 @@ from .pyerl_gaussian_process import *
 __all__ = [
     "common",
     "covariance",
+    "geometry",
     "VanillaGaussianProcess",
     "Mapping",
     "LidarGaussianProcess2D",
